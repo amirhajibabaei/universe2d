06908463b7d84ba2121b1d00b7c27216041b4b76
amir <amir@bose.(none)>
2018-04-20 14:23:35 +0900

    when file doesn't exist

--- scripts/scripts/analyse_scalars.py
+++ scripts/scripts/analyse_scalars.py
@@ -1,5 +1,9 @@
 import numpy as np
 import sys
+import os
+if not os.path.isfile(sys.argv[1]):
+    print "# "+sys.argv[1]+" doesn't exist!"
+    sys.exit()
 t,u,p,o1,o2 = np.loadtxt(sys.argv[1],float,unpack=True)
 o = o1**2+o2**2
 ndat = len(t)
